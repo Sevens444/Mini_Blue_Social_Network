2e7e87d736eff6138ad1a150301506fb4e1cad42
NoutHP <kola.l@bk.ru>
2024-05-26 13:00:14 +0300

    Незначительные доработки

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@db:5432/social_network')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@db:5432/mbsn')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
