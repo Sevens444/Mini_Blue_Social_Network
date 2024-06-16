b3325c3b953f0c36cb72d4de9283bf4a6b6aa41f
Nikolay Obr <kola.l@bk.ru>
2024-06-16 19:23:30 +0300

    Stable version 2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from sqlalchemy import text
-from extensions import db, login, migrate
+from extensions import *
 import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "777"
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:0123@localhost:5432/mbsn')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
-    # flask db init
-    # flask db migrate -m "Add messages model"
-    # flask db upgrade
     login.init_app(app)
 
     from routes import bp as main_bp
@@ -21,11 +18,11 @@ def create_app():
 
     return app
 
-if 1 == 2:
-    with app.app_context():
-        # db.session.create_all()
-        result = db.session.execute(text('SELECT * FROM users'))
-        print([row for row in result])
+# if 1 == 2:
+#     with app.app_context():
+#         # db.session.create_all()
+#         result = db.session.execute(text('SELECT * FROM users'))
+#         print([row for row in result])
 
 app = create_app()
 
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-flask db upgrade
-#read -p "Press enter to continue"
-exec gunicorn -b 0.0.0.0:5000 "app:create_app()"
-#read -p "Press enter to continue"
