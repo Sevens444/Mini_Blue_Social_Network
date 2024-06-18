789ab235d763a8356515585df660fd7e7a003181
Nick Obraztsov <kola.l@bk.ru>
2024-06-18 22:10:47 +0300

    Доработки для запуска через docker container или localhost

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "777"
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:0123@localhost:5400/mbsn')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -17,12 +17,6 @@ def create_app():
     app.register_blueprint(main_bp)
 
     return app
-
-# if 1 == 2:
-#     with app.app_context():
-#         # db.session.create_all()
-#         result = db.session.execute(text('SELECT * FROM users'))
-#         print([row for row in result])
 
 app = create_app()
 
